5e534c234a68ccd1dcc38af1230995b4ee15cff0
Moritz Wilksch <moritz.wilksch@quantco.com>
2024-11-18 10:55:16 +0100

    update slime target pane id

--- init.lua
+++ init.lua
@@ -376,7 +376,7 @@ require('lazy').setup({
             vim.g.slime_cell_delimiter = '# %%'
             vim.g.slime_default_config = {
                 socket_name = 'default',
-                target_pane = '1'
+                target_pane = '2'
             }
             vim.g.slime_bracketed_paste = 1
             vim.g.slime_dont_ask_default = 1
